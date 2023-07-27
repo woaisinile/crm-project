a00fe354578275afa1f6d9b21266c780bcc40f6f
gen_l <962317092@qq.com>
2023-07-27 21:24:40 +0800

    布局完善

--- src/views/layout/MainLayout.js
+++ src/views/layout/MainLayout.js
@@ -1,21 +1,90 @@
 import React, { useState } from 'react';
+// 图标
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
     UploadOutlined,
     UserOutlined,
     VideoCameraOutlined,
+    AppstoreOutlined,
+    MailOutlined,
+    SettingOutlined
 } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
 import './MainLayout.css'
 
-const { Header, Sider, Content } = Layout;
+const {
+    Header,
+    Sider,
+    Content
+} = Layout;
+
+// 顶部导航栏
+const items = [
+    {
+        label: 'Navigation One',
+        key: 'mail',
+        icon: <MailOutlined />,
+    },
+    {
+        label: 'Navigation Two',
+        key: 'app',
+        icon: <AppstoreOutlined />,
+        disabled: true,
+    },
+    {
+        label: 'Navigation Three - Submenu',
+        key: 'SubMenu',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                type: 'group',
+                label: 'Item 1',
+                children: [
+                    {
+                        label: 'Option 1',
+                        key: 'setting:1',
+                    },
+                    {
+                        label: 'Option 2',
+                        key: 'setting:2',
+                    },
+                ],
+            },
+            {
+                type: 'group',
+                label: 'Item 2',
+                children: [
+                    {
+                        label: 'Option 3',
+                        key: 'setting:3',
+                    },
+                    {
+                        label: 'Option 4',
+                        key: 'setting:4',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: (
+            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                Navigation Four - Link
+            </a>
+        ),
+        key: 'alipay',
+    },
+];
 
 const MainLayout: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+
+    // 顶部菜单栏选中状态
+    const [current, setCurrent] = useState('mail');
 
     return (
         <Layout className={'mainLayout'}>
@@ -47,16 +116,19 @@ const MainLayout: React.FC = () => {
             </Sider>
             <Layout>
                 <Header style={{ padding: 0, background: colorBgContainer }}>
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
+                    <div className="header-content">
+                        <Button
+                            type="text"
+                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{
+                                fontSize: '16px',
+                                width: 64,
+                                height: 64,
+                            }}
+                        />
+                        <Menu selectedKeys={[current]} mode="horizontal" items={items} />
+                    </div>
                 </Header>
                 <Content
                     style={{
