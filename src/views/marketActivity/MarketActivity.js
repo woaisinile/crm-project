cb755b301bc0b25b088813635f6a854935e2a7ca
gen_l <962317092@qq.com>
2023-08-14 22:14:30 +0800

    单选实现，删除多余的函数

--- src/views/marketActivity/MarketActivity.js
+++ src/views/marketActivity/MarketActivity.js
@@ -141,12 +141,7 @@ const MarketActivity = () => {
     const rowSelection = {
         onChange: (selectedRowKeys, selectedRows) => {
             console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-        },
-        getCheckboxProps: (record) => ({
-            disabled: record.name === 'Disabled User',
-            // Column configuration not to be checked
-            name: record.name,
-        }),
+        }
     };
 
     return (
